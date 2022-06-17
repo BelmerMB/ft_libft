842be6703683ba19c9f890967b7b3690e0d1cbf8
emetras- | elmer metras <emetras-@student.42sp.org.br>
2022-06-17 03:25:52 +0200

    Makefile and libft.h updated

--- libft/libft.h
+++ libft/libft.h
@@ -6,12 +6,14 @@
 /*   By: emetras- <emetras-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 17:37:10 by emetras-          #+#    #+#             */
-/*   Updated: 2022/06/16 13:26:12 by emetras-         ###   ########.fr       */
+/*   Updated: 2022/06/16 20:18:21 by emetras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# define INT_MIN  (-2147483648)
+# define INT_MAX   (2147483647)
 # include <stdlib.h>
 
 int		ft_isalpha(int c);
@@ -34,5 +36,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_atoi(const char *nptr);
+char	*ft_strdup(const char *s);
 
 #endif
