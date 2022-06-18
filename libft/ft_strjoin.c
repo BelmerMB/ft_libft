539e448b77b7019eb1a7424a895c1acb9cb41a1e
emetras- | elmer metras <emetras-@student.42sp.org.br>
2022-06-18 06:37:01 +0200

    ft_substr and join ok

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,18 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emetras- <emetras-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/02 23:05:52 by emetras-          #+#    #+#             */
-/*   Updated: 2022/06/17 03:29:45 by emetras-         ###   ########.fr       */
+/*   Created: 2022/06/18 05:24:34 by emetras-          #+#    #+#             */
+/*   Updated: 2022/06/18 06:12:06 by emetras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_toupper(int c)
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	if (c >= 'a' && c <= 'z')
-		return (c -32);
-	return (c);
+	size_t	size;
+	size_t	index;
+	char	*ptr;
+
+	index = 0;
+	size = ft_strlen(s1) + ft_strlen(s2);
+	ptr = (char *) ft_calloc(size +1, sizeof(char));
+	if (!ptr)
+		return (NULL);
+	while (*s1)
+		ptr[index++] = *s1++;
+	while (*s2)
+		ptr[index++] = *s2++;
+	ptr[index] = '\0';
+	return (ptr);
 }
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -1,18 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emetras- <emetras-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/02 23:05:52 by emetras-          #+#    #+#             */
-/*   Updated: 2022/06/17 03:29:45 by emetras-         ###   ########.fr       */
+/*   Created: 2022/06/17 03:34:39 by emetras-          #+#    #+#             */
+/*   Updated: 2022/06/18 06:12:00 by emetras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_toupper(int c)
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	if (c >= 'a' && c <= 'z')
-		return (c -32);
-	return (c);
+	size_t	s_len;
+	char	*ptr;
+
+	s_len = ft_strlen(s);
+	if (start > s_len)
+	{
+		len = 1;
+		start = 0;
+	}
+	else if (len + start > s_len)
+		len = s_len - start + 1;
+	else
+		len = len + 1;
+	ptr = malloc(len * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	ft_strlcpy(ptr, &s[start], len);
+	return (ptr);
 }
